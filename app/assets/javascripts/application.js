5606299354451a71c53a5bd78257541667ed2d89
Tatsupon <tatsuponbipanya@gmail.com>
2020-01-03 19:53:58 +0900

    Finish layout and routes

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,5 +11,9 @@
 // about supported directives.
 //
 //= require rails-ujs
+//= require jquery
+//= require jquery_ujs
+//= require jquery.turbolinks
 //= require turbolinks
 //= require_tree .
+//= require bootstrap-sprockets
---
+++ spec/controllers/users_controller_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.describe UsersController, type: :controller do
+
+  describe "GET #new" do
+    it "should get signup" do
+      get :new
+      expect(response).to be_success
+    end
+  end
+
+end
