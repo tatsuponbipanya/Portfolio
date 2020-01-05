43b5a5ef8cb661313b71f6bc55180a7fd9acc605
Tatsupon <tatsuponbipanya@gmail.com>
2020-01-06 01:33:24 +0900

    Finish user signup

---
+++ spec/requests/users_signups_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe "UsersSignups", type: :request do
+  it "valid signup information" do
+    get signup_path
+    expect {
+      post users_path, params: { user: { name:  "Example User",
+                                         email: "user@example.com",
+                                         password:              "password",
+                                         password_confirmation: "password" } }
+    }.to change(User, :count).by(1)
+    redirect_to @user
+    follow_redirect!
+    # Test
+    assert_template 'users/show'
+    #assert is_logged_in?
+  end
+end
