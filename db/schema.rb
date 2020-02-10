31b75ebf3b06fd2b5e6e419e550daf23579a0bb9
Tatsupon <tatsuponbipanya@gmail.com>
2020-02-11 08:16:04 +0900

    Fix-Rakuten

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200117173853) do
+ActiveRecord::Schema.define(version: 20200210214436) do
+
+  create_table "amazons", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "goods", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "likes", force: :cascade do |t|
     t.integer "user_id", null: false
