ae26e3ac83687e20428607d9a60976189d8d87bb
tatsumajiro <tatsuponbipanya@gmail.com>
2026-06-02 17:35:10 +0900

    いいね機能のテスト追加

--- spec/controllers/likes_controller_spec.rb
+++ spec/controllers/likes_controller_spec.rb
@@ -2,5 +2,21 @@
 
 require 'rails_helper'
 
-RSpec.describe LikesController, type: :controller do
+    RSpec.describe LikesController, type: :controller do
+  let(:user) { User.create(name: "Test", email: "test@a.com", password: "password") }
+  let(:micropost) { Micropost.create!(content: "test", user_id: user.id) }
+
+  before do
+    # セッションにユーザーIDを入れて、ログイン状態をエミュレートする
+    session[:user_id] = user.id
+  end
+
+  describe 'POST #create' do
+    it 'いいねが作成されること' do
+      # paramsのキーが :micropost_id であることを確認
+      post :create, params: { micropost_id: micropost.id }
+      
+      expect(Like.count).to eq(1)
+    end
+  end
 end
